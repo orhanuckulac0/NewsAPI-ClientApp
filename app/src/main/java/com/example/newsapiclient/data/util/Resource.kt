77f24755cf27c535d757c37dcc79b46ec510c675
Orhan Uckulac <orhanuckulac0@gmail.com>
2022-12-30 03:31:11 +0300

    2. commit - complete the domain layer of the project

---
+++ app/src/main/java/com/example/newsapiclient/data/util/Resource.kt
@@ -0,0 +1,12 @@
+package com.example.newsapiclient.data.util
+
+// define 3 classes for api response state
+// a generic class that contains data and status about loading the data. //
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+}
--- app/src/main/java/com/example/newsapiclient/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapiclient/domain/repository/NewsRepository.kt
@@ -1,6 +1,19 @@
 package com.example.newsapiclient.domain.repository
 
-interface NewsRepository {
-    // TODO first define models, and then create functions below:
+import com.example.newsapiclient.data.model.APIResponse
+import com.example.newsapiclient.data.model.Article
+import com.example.newsapiclient.data.util.Resource
+import kotlinx.coroutines.flow.Flow
 
+interface NewsRepository {
+    // functions for network communication
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
+
+    // functions related to local db
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    // in the viewModel class, we will collect this stream of data flow and emit it as live data
+    // since this fun returns a data stream, don't need to write as suspend fun
+    fun getSavedNews(): Flow<List<Article>>
 }
